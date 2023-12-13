ec1e6f95c3c3386261807a8dc369bab947f08dc0
zhangliang <liang_zhang@cems-ltd.com>
2023-12-13 17:52:35 +0800

    init project

---
+++ Lemonui-wpf/Controls/MyWrapPanel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Lemonui_wpf.Controls
+{
+    public class MyWrapPanel:FrameworkElement
+    {
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            drawingContext.DrawRectangle(Brushes.Red, new Pen(Brushes.Green, 1), new Rect(0, 0, 200, 100));
+        }
+    }
+}
